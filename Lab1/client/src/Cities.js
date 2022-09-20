03e5e31aed07ebde02b04eebaedd680b47c8a5ec
cnryng <connor-young@outlook.com>
2022-09-20 11:50:17 -0700

    moved labs to different folders

--- Lab1/client/src/Cities.js
+++ Lab1/client/src/Cities.js
@@ -2,12 +2,12 @@ import React, { useEffect, useState } from 'react'
 import City from './City'
 function Cities() {
 
-    const [cities, setCities] = useState([])
+    const [cities, setCities] = useState([]) //cities initially holds empty array and is updated by the setCities function
     const url = "http://localhost:5000/cities"
     useEffect(() => {
         fetch(url)
             .then((resp) => { return resp.json() })
-            .then((jsonedResp) => { setCities(jsonedResp)})
+            .then((jsonedResp) => { setCities(jsonedResp)}) //cities will now have the value of jsonedResp
     }, [])
 
     return (
