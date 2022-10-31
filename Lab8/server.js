318a0f3fc7722a3f9b362d30e669581a0e097fad
Connor Young <connor-young@outlook.com>
2022-10-31 10:21:44 -0700

    switched to async/await mongoose queries instead of using .exec() callback

--- Lab8/server.js
+++ Lab8/server.js
@@ -3,11 +3,13 @@ const mongoose = require('mongoose');
 const path = require('path');
 const bodyParser = require('body-parser');
 const https = require('https');
+const pokemonErrors = require("./errors.js")
 
 const app = express();
 const port = 5000;
 const dbURL = "mongodb+srv://test-user:9pmeJjRNqtysbXZF@testcluster.lcqmg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const { Schema } = mongoose;
+const { PokemonBadRequest, PokemonNotFoundError } = pokemonErrors;
 
 let pokemonSchema;
 let pokemonModel;
@@ -118,53 +120,53 @@ app.listen(process.env.PORT || 5000, async () => {
     })
 })
 
-app.get('/api/v1/pokemons', (req, res) => { // - get all the pokemons after the 10th. List only Two.
+app.get('/api/v1/pokemons', asyncWrapper (async (req, res) => { // - get all the pokemons after the 10th. List only Two.
     function isNumber(n) { return !isNaN(parseFloat(n)) && !isNaN(n - 0) }
 
     if ((req.query.after && !isNumber(req.query.after)) || (req.query.count && !isNumber(req.query.count))) {
-        res.json({msg: `after = ${req.query.after}, count = ${req.query.count}; after and count query parameters must be numbers`});
-        return;
+        throw new PokemonBadRequest(`after = ${req.query.after}, count = ${req.query.count}; after and count query parameters must be numbers`);
     }
 
-    pokemonModel.find().skip(req.query.after).limit(req.query.count).exec((err, result) => {
-        if (err) {
-            res.json(getMongooseErrorMessage(err, req));
-        } else {
-            res.json(result);
-        }
-    })
-})
+    const result = await pokemonModel.find().skip(req.query.after).limit(req.query.count);
 
-app.post('/api/v1/pokemon', bodyParser.json(), (req, res) => { // - create a new pokemon
-    console.log(req.body);
-    let responseBody;
-    pokemonModel.create(req.body, (err, opRes) => {
-        if (err) {
-            res.json(getMongooseErrorMessage(err, req));
-        } else {
-            console.log(opRes);
-            res.json({msg: "Successfully inserted new pokemon"});
-        }
-    })
-})
+    if (result.length === 0) {
+        throw new PokemonNotFoundError(`No pokemon found with current filter values`);
+    }
 
-app.get('/api/v1/pokemon/:id', (req, res) => { // - get a pokemon
-    console.log(req.params.id);
-    pokemonModel.find({ id: req.params.id }, (err, doc) => {
-        if (err) {
-            console.error(err)
-            res.json({msg: "db reading .. err.  Check with server devs"})
-        } else if (doc.length === 0){
-            console.log(doc);
-            res.json({msg: `Pokemon with id ${req.params.id} does not exists`});
-        } else {
-            console.log(doc);
-            res.json(doc);
-        }
-    })
-})
+    res.json(result);
 
-app.get('/api/v1/pokemonImage/:id', async (req, res) => { // - get the url of a pokemon
+}))
+
+app.post('/api/v1/pokemon', bodyParser.json(), asyncWrapper (async (req, res) => { // - create a new pokemon
+    await pokemonModel.create(req.body);
+    res.json({msg: "Successfully inserted pokemon"});
+}))
+
+app.get('/api/v1/pokemon/:id', asyncWrapper(async (req, res) => { // - get a pokemon
+
+    const pokemon = await pokemonModel.find({ id: req.params.id });
+
+    if (pokemon.length === 0) {
+        throw new PokemonNotFoundError(`Pokemon with id ${req.params.id} does not exists`);
+    }
+
+    res.json(pokemon);
+
+    // pokemonModel.find({ id: req.params.id }, (err, doc) => {
+    //     if (err) {
+    //         console.error(err)
+    //         res.json({msg: "db reading .. err.  Check with server devs"})
+    //     } else if (doc.length === 0){
+    //         console.log(doc);
+    //         res.json({msg: `Pokemon with id ${req.params.id} does not exists`});
+    //     } else {
+    //         console.log(doc);
+    //         res.json(doc);
+    //     }
+    // })
+}))
+
+app.get('/api/v1/pokemonImage/:id', asyncWrapper(async (req, res) => { // - get the url of a pokemon
     function idToThreeDigitString(id) {
         if (Math.floor(id / 10) >= 10) {
             return id.toString();
@@ -178,9 +180,9 @@ app.get('/api/v1/pokemonImage/:id', async (req, res) => { // - get the url of a 
         url: "https://raw.githubusercontent.com/fanzeyi/pokemon.json/master/images/"
             + idToThreeDigitString(req.params.id) + ".png"
     })
-})
+}))
 
-app.put('/api/v1/pokemon/:id', bodyParser.json(), (req, res) => { // - upsert a whole pokemon document
+app.put('/api/v1/pokemon/:id', bodyParser.json(), asyncWrapper((req, res) => { // - upsert a whole pokemon document
     pokemonModel.updateOne({ id: req.params.id }, req.body, { upsert: true, runValidators: true, new: true }, (err, opRes) => {
         if (err) res.json(getMongooseErrorMessage(err, req));
         else if (opRes.upsertedCount > 0) {
@@ -191,9 +193,9 @@ app.put('/api/v1/pokemon/:id', bodyParser.json(), (req, res) => { // - upsert a 
             res.json({msg: `Successfully updated pokemon with id ${req.params.id}`})
         }
     });
-})
+}))
 
-app.patch('/api/v1/pokemon/:id', bodyParser.json(), (req, res) => { // - patch a pokemon document or a portion of the pokemon document
+app.patch('/api/v1/pokemon/:id', bodyParser.json(), asyncWrapper((req, res) => { // - patch a pokemon document or a portion of the pokemon document
     console.log(req.body);
     pokemonModel.updateOne({ id: req.params.id }, req.body, { runValidators: true }, (err, opRes) => {
         if (err) res.json(getMongooseErrorMessage(err, req));
@@ -204,9 +206,9 @@ app.patch('/api/v1/pokemon/:id', bodyParser.json(), (req, res) => { // - patch a
             res.json({msg: `Patch failed. Pokemon with id ${req.params.id} does not exist`})
         }
     })
-})
+}))
 
-app.delete('/api/v1/pokemon/:id', (req, res) => { // - delete a  pokemon
+app.delete('/api/v1/pokemon/:id', asyncWrapper((req, res) => { // - delete a  pokemon
     pokemonModel.deleteOne({ id: req.params.id },  (err, opRes) => {
         if (err) {
             console.error(err);
@@ -219,12 +221,18 @@ app.delete('/api/v1/pokemon/:id', (req, res) => { // - delete a  pokemon
             res.json({msg: `Pokemon with id ${req.params.id} successfully deleted`});
         }
     })
-})
+}))
 
-app.get('/api/doc', (req, res) => {
+app.get('/api/doc', asyncWrapper((req, res) => {
     res.sendFile(path.join(__dirname, '/docs.html'))
-})
+}))
 
 app.all('*', (req, res) => {
     res.status(404).json({msg: "Improper request"});
+})
+
+app.use((err, req, res, next) => {
+    console.error("Overrided default Express error handler...");
+    console.error(err.stack);
+    res.status(500).json({msg: err.message})
 })
