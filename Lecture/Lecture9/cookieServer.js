0aee5107e246bfd7ed5e9f3ba01b5a7c25c33c26
Connor Young <connor-young@outlook.com>
2022-11-01 09:19:38 -0700

    added csrf protection

--- Lecture/Lecture9/cookieServer.js
+++ Lecture/Lecture9/cookieServer.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const csurf = require("tiny-csrf");
+const session = require('express-session');
 const app = express();
 
 app.listen(5000, () => console.log('Server is listening on port 5000'));
 
-app.use(cookieParser());
+//app.use(cookieParser());
+app.use(express.urlencoded());
+app.use(cookieParser("cookie-parser-secret"));
+app.use(session({ secret: "keyboard cat" }));
+app.use(csurf("123456789iamasecret987654321look"));
+
 app.get('/', (req, res) => {
     res.cookie("sea", "blue"); //can set key-value of a cookie
     res.cookie("land", "green", { httpOnly: true, maxAge: 6000 }); //third parameter is to add options
@@ -14,17 +21,30 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 })
 
+// app.get('/login', (req, res) => {
+//     res.send("<form action='/login' method='POST'>" +
+//             "<input type='text' name='username' value='admin' placeholder='username'>" +
+//             "<input type='password' name='password' value='1234' placeholder='password'>" +
+//             "<button type='submit'>Login</button>" +
+//         "</form>"
+//     );
+// })
+
 app.get('/login', (req, res) => {
-    res.send("<form action='/login' method='POST'>" +
-            "<input type='text' name='username' value='admin' placeholder='username'>" +
-            "<input type='password' name='password' value='1234' placeholder='password'>" +
-            "<button type='submit'>Login</button>" +
-        "</form>"
-    );
+    res.send(`
+  <form action='/login' method='POST'>
+    <input type='text' name='username' value='admin' placeholder='username'>
+    <input type='password' name='password' value='1234' placeholder='password'>
+    <button type='submit'>Login</button>
+    <input type='hidden' name='_csrf' value='${req.csrfToken()}'>
+  </form>
+  <form action="http://localhost:5000/admin">
+    <button>Click me if you want to redeem a prize money</button>
+  </form>
+  `);
 })
 
 //send credentials via post req.body using form-urlencoded
-app.use(express.urlencoded());
 app.post('/login', (req, res) => {
     console.log(req.body);
     if (req.body.username === 'admin' && req.body.password === '1234') {
