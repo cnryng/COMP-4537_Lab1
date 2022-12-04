67b1500491833658cc27fb1c21b8395a306ea576
Connor Young <connor-young@outlook.com>
2022-12-03 20:33:16 -0800

    added Routing

--- Assignment3/client/src/index.js
+++ Assignment3/client/src/index.js
@@ -2,10 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App'
+import AdminDashboard from "./AdminDashboard";
+import { Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <>
-    <App />
+      <React.StrictMode>
+          <BrowserRouter>
+              <Routes>
+                  <Route path="/" element={<App />} />
+                  <Route path="/admin" element={<AdminDashboard isAdmin={true}/>} />
+              </Routes>
+          </BrowserRouter>
+      </React.StrictMode>
   </>
 );
