980ac037434e4aa88e5430e15e0f0c1b2ea87cfb
Connor Young <connor-young@outlook.com>
2022-12-03 21:43:26 -0800

    added api reports to dashboard

--- Assignment3/client/src/AdminDashboard.js
+++ Assignment3/client/src/AdminDashboard.js
@@ -1,29 +1,60 @@
 import React, { useEffect, useState } from 'react'
-import Pagination from './Pagination';
 import axios from 'axios'
 import Cookies from 'js-cookie';
+import Card from 'react-bootstrap/Card';
 
-function AdminDashboard( { isAdmin } ) {
+function AdminDashboard() {
 
+    const [isAdmin, setIsAdmin] = useState(false);
     const [apiRequests, setApiRequests] = useState([]);
+    const [fourHundredRequests, setFourHundredRequests] = useState(0);
 
     useEffect(() => {
-        axios({
+        const fetchData = async () => {
+        const checkIfAdmin = await axios({
             method: 'get',
-            url: 'http://localhost:5001/api/v1/requests',
+            url: 'http://localhost:5001/api/v1/isAdmin',
             headers: {
                 "token": Cookies.get('token')
             }
-        }).then(res => {
-            console.log(res.data)
-            setApiRequests(res.data)
-        }).then(() => console.log(apiRequests))
-            .catch(err => console.log("err", err))
+        })
+        setIsAdmin(checkIfAdmin.data);
+        console.log(checkIfAdmin.data);
+        const apiRequests = await axios({
+            method: 'get',
+            url: 'http://localhost:5001/api/v1/uniqueRequests',
+            headers: {
+                "token": Cookies.get('token')
+            }
+        })
+        setApiRequests(apiRequests.data);
+        const get400Requests = await axios({
+            method: 'get',
+            url: 'http://localhost:5001/api/v1/400requests',
+            headers: {
+                "token": Cookies.get('token')
+            }
+        })
+        setFourHundredRequests(get400Requests.data);
+        console.log(get400Requests.data);
+        }
+        fetchData().catch(err => console.log(err))
     }, [])
 
     return (
         <div>
-            { isAdmin ? (<h1>Admin</h1>) : (<h1>Not Admin</h1>)}
+            { isAdmin ? (
+                <div>
+                    <h1>Admin Dashboard</h1>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Number of unique requesters</Card.Title>
+                            <Card.Text>{apiRequests.length}</Card.Text>
+                        </Card.Body>
+                    </Card>
+                </div>
+                )
+                : (<h1>Not Admin</h1>)}
         </div>
     )
 }
