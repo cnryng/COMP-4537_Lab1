a00b284b0d71f8ef8026709526d1c90de5c6f6d5
Connor Young <connor-young@outlook.com>
2022-12-03 20:32:45 -0800

    added schema for ApiRequests

---
+++ Assignment3/server/apiRequestModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const schema = new mongoose.Schema({
+    request: {
+        type: String,
+        required: true,
+    },
+    status: {
+        type: Number,
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    token: {
+        type: String,
+        required: true,
+    }
+})
+
+module.exports = mongoose.model('apiRequest', schema) //pokeUser is the name of the collection in the db
